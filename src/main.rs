ca1ff0f72a4beb9dc2ffbbbed7fa758c1c19c418
从零开始的程序员生活 <793932470@qq.com>
2024-01-05 01:35:05 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -124,6 +124,7 @@ fn main() {
             check_relative_cursor_position,
             place_brick_system,
             export_placed_bricks,
+            touch_system,
         ))
         .run();
 }
@@ -549,4 +550,33 @@ fn export_placed_bricks(
         let _ = std::fs::write("./output.json", json_str);
     };
    
+}
+
+
+fn touch_system(touches: Res<Touches>) {
+    for touch in touches.iter_just_pressed() {
+        info!(
+            "just pressed touch with id: {:?}, at: {:?}",
+            touch.id(),
+            touch.position()
+        );
+    }
+
+    for touch in touches.iter_just_released() {
+        info!(
+            "just released touch with id: {:?}, at: {:?}",
+            touch.id(),
+            touch.position()
+        );
+    }
+
+    for touch in touches.iter_just_canceled() {
+        info!("canceled touch with id: {:?}", touch.id());
+    }
+
+    // you can also iterate all current touches and retrieve their state like this:
+    for touch in touches.iter() {
+        info!("active touch: {:?}", touch);
+        info!("  just_pressed: {}", touches.just_pressed(touch.id()));
+    }
 }
